89aa781e8a97b011c8d6df79617532a225e8d427
Madeoliveirarocha <madeoliveirarocha@gmail.com>
2021-11-26 20:36:11 +0000

    exercicio1

--- Program.cs
+++ Program.cs
@@ -1,36 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dotnetcore
 {
     class Program
     {
-        // Exercício 1:
-        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
-        // - Imprimir no console para cada aluno
-        //      - Nome do aluno
-        //      - Caso média maior ou igual 7:
-        //          - Passou no ano letivo
-        //          - Não passou no ano letivo
-        // Requisítos:
-        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
-        // - Utilizar For ou Foreach
-
-
-        // Exercício 2:
-        // - Fazer um programa que cadastre pessoas, contendo nome e idade;
-        // - Imprimir a media de idade das pessoas
-        // - Imprimir o nome e idade da pessoa mais velha
-        // - Imprimir o nome e idade da pessoa mais nova
-        // - Imprimir a quantidade de pessoas com maior idade
-        // Requisítos:
-        // - Utilizar List (Avg, Max, Min, Where)
-        // - Utilizar Classe Pessoa
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando Programa");
+            Console.WriteLine("Medias:");
 
-            var xNotasZuqui = new int[] { 7, 6, 5, 3 };
-            var xNotasBronza = new int[] { 8, 9, 10, 8 };
+            var xNotasZuqui = new List<int> { 7, 6, 5, 3 };
+            var xNotasBronza = new List<int> { 8, 9, 10, 8 };
+
+            var xMediazuqi = CalcularMedia(xNotasZuqui);
+            Console.WriteLine("Media Zuqui: " + xMediazuqi);
+            
+            if (xMediazuqi<7) 
+            {
+                Console.WriteLine("Reprovado");
+            } 
+            else
+            {
+                if( xMediazuqi> 7 )
+                    Console.WriteLine("Aprovado");
+            }
+            
+
+            var mediabronza = CalcularMedia(xNotasBronza);
+            Console.WriteLine("MediaBronza " + mediabronza );
+            if (mediabronza < 7) {
+                Console.WriteLine("Reprovado");
+            }
+            else
+            {
+                if(( mediabronza > 7 ))
+                Console.WriteLine("Aprovado"); 
+            }
+
+            
+        }
+
+        static float CalcularMedia(List<int> notas)
+        {
+            var total = 0;
+
+            foreach (var nota in notas)
+            {
+                total += nota; 
+                
+            }
+            return total / notas.Count;            
         }
     }
 }
